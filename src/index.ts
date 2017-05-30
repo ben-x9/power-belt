88c67da22f85447d168ed3b1bde68933a861c2c8
Ben Lambert <ben@joyxer.com>
2017-05-30 17:04:41 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,3 @@ export type OneOrMore<T> = T | List<T>
 export const list = <T>(...x: Array<T>): List<T> => x
 export const dict = <T>(object: Obj<T>): Dict<T> => object
 export const record = <T>(object: T): Record<T> => object
-
-
-export type Ar<T> = ReadonlyArray<T>
-export type OneOrMore<T> = T | Ar<T>
