84e9fdc6cee45df6f5fa896433a959ff85d3db0a
Ben Lambert <ben@joyxer.com>
2017-05-30 17:14:15 +0900

    change list to readonly array

--- src/index.ts
+++ src/index.ts
@@ -19,10 +19,7 @@ export const log = (...xs: Array<any>) => {
   return last(xs)
 }
 
-export type List<T> = {
-  readonly [index: number]: T
-  readonly length: number
-}
+export type List<T> = ReadonlyArray<T>
 
 export type Dict<T> = {
   readonly [key: string]: T
@@ -36,3 +33,51 @@ export type OneOrMore<T> = T | List<T>
 export const list = <T>(...x: Array<T>): List<T> => x
 export const dict = <T>(object: Obj<T>): Dict<T> => object
 export const record = <T>(object: T): Record<T> => object
+
+
+// const set = <T, K extends keyof T>(thing: T, prop: K, value: T[K]): T => {
+//   return thing
+// }
+
+// const myMap: ReadMap<number> = {
+//   a: 1,
+//   b: 2,
+//   c: 3
+// }
+// const myArray: ReadArray<number> = [1, 2, 3]
+
+
+// const myNewMap = set(myMap, "a", 5)
+// const myNewArray = set(myArray, 0, 5)
+
+
+// type T1 = keyof {[key: string]: number}
+// type T2 = keyof {[key: number]: number}
+
+
+
+
+
+
+// export const record = <T>(value: T) => value as Readonly<T>
+// export const list = <T>(value: T[]) => value as ReadArray<T>
+
+// const a = [1, 2, 3]
+// type ka<T> = keyof ReadArray<T>
+
+// type Set = <R, K extends keyof R>(rec: R, key: K, val: R[K]) => R
+// type Set = <T>(vec: Array<T>, index: number, val: R[K]) => R
+
+// const set: Set = (rec: any, key: any, val: any) => {
+
+// }
+
+
+
+// type Wrapper<T> = {
+//     set<K extends keyof T>(k: K, v: T[K]) : IWrapper<T>
+//     setIn<K extends keyof T, K2 extends keyof T[K]>(k: K, k2: K2, v: T[K][K2]) : IWrapper<T>
+//     setIn<K extends keyof T, K2 extends keyof T[K], K3 extends keyof T[K][K2]>(k: K, k2: K2, k3: K3, v: T[K][K2][K3]): IWrapper<T>
+//     setIn<K extends keyof T, K2 extends keyof T[K], K3 extends keyof T[K][K2], K4 extends keyof T[K][K2][K3]>(k: K, k2: K2, k3: K3, k4: K4, v: T[K][K2][K3][K4]): IWrapper<T>
+//    // .......
+// }
