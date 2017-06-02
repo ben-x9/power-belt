78785d8ad907c998215b99d68cc9d20d519dd2e4
Ben Lambert <ben@joyxer.com>
2017-06-02 13:33:33 +0900

    make set work with arrays

--- src/index.ts
+++ src/index.ts
@@ -24,31 +24,45 @@ export const debug = <T>(expr: T): T => {
   return expr
 }
 
+// immutable types
 export type List<T> = ReadonlyArray<T>
-
-export type Dict<T> = {
-  readonly [key: string]: T
-}
+export type Dict<T> = {readonly [key: string]: T}
 export type Obj<T> = Dict<T>
-
 export type Record<T> = Readonly<T>
-
-export type OneOrMore<T> = T | List<T>
-
 export const list = <T>(...x: Array<T>): List<T> => x
 export const dict = <T>(object: Obj<T>): Dict<T> => object
 export const record = <T>(object: T): Record<T> => object
+
+export type OneOrMore<T> = T | List<T>
 
 export type Update<ActionT> = (update: ActionT) => void
 export type Guid = string
 export type Nothing = null | undefined
 export type Maybe<T> = T | Nothing
 
-export function set<T>(obj: T, props: Partial<T>): T
-export function set<T, K extends keyof T>(obj: T, key: K, val: T[K]): T
-export function set<T, K extends keyof T>
-    (obj: T, propsOrKey: Partial<T> | K, val?: T[K]) {
-  return Object.assign(obj, isString(propsOrKey) ?
-    {[propsOrKey as string]: val} :
-    propsOrKey)
+export function set<T>(object: T, props: Partial<T>): T
+export function set<T, K extends keyof T>(object: T, key: K, val: T[K]): T
+export function set<T>(list: List<T>, index: number, val: T): List<T>
+export function set<T, K extends keyof T>(
+    objOrList: T | List<T>,
+    propsOrKeyOrIndex: Partial<T> | K | number,
+    val?: T[K] | T) {
+  if (isList(objOrList)) {
+    const list = objOrList as List<T>
+    const index = propsOrKeyOrIndex as number
+    const $val = val as T
+    const $list = list.slice() // clone the list
+    $list.splice(index, 1, $val) // replace the val at index
+    return $list
+  } else {
+    const object = objOrList
+    const propsOrKey = propsOrKeyOrIndex
+    const $val = val as T[K]
+    return Object.assign({}, object, isString(propsOrKey) ?
+      {[propsOrKey as string]: $val} :
+      propsOrKey)
+  }
 }
+
+export interface DeepList<T> extends List<T | DeepList<T>> {}
+export type Deep<T> = T | DeepList<T>
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
 import {assert} from "chai"
-import {isArray} from "src"
+import {isArray, set, record} from "src"
 
 describe("isArray", () => {
   it("should return true when passed an array", () => {
@@ -9,5 +9,26 @@ describe("isArray", () => {
     assert.strictEqual(isArray({}), false)
     assert.strictEqual(isArray(""), false)
     assert.strictEqual(isArray(20), false)
+  })
+})
+
+describe("set", () => {
+  it("should set a property on an object when passed an object, a key and a value", () => {
+    const object = {a: 1, b: 2, c: 3}
+    const $object = set(object, "a", 11)
+    assert.notEqual($object, object)
+    assert.deepEqual($object, {a: 11, b: 2, c: 3})
+  })
+  it("should set properties on the first object when passed two objects", () => {
+    const object = {a: 1, b: 2, c: 3}
+    const $object = set(object, {a: 11, b: 22})
+    assert.notEqual($object, object)
+    assert.deepEqual($object, {a: 11, b: 22, c: 3})
+  })
+  it("should set an array value at index when passed an array", () => {
+    const array = [1, 2, 3]
+    const $array = set(array, 1, 22)
+    assert.notEqual($array, array)
+    assert.deepEqual($array, [1, 22, 3])
   })
 })
